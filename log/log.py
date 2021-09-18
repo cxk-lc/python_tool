3d28f27e646e06ffab2184cd18726ace5557f171
cxk <cxk@originqc.com>
2021-09-18 19:29:47 +0800

    log module

--- log/log.py
+++ log/log.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+import logging
+from logging.config import fileConfig
+
+fileConfig('logging.conf')
+logger = logging.getLogger('file')  # 同时输出到文件与控制台
+
+if __name__ == '__main__':
+    logger.debug('test')
